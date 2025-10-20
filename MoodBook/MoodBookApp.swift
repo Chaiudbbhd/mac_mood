c196dc7d7a78cb026e0e7c8c4f17f5f715674f82
Lakshmi Prasanna Kumar <your-email@example.com>
2025-10-20 09:53:22 +0530

    Initial Commit

---
+++ MoodBook/MoodBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodBookApp.swift
+//  MoodBook
+//
+//  Created by Ganteda Lakkshmi Prasanna kumaar on 20/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
